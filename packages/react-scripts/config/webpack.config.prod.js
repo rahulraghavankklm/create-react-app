92c9c2649ce467ca6bfa76e2b87cab82ae216a8c
Rahul Raghavan <rahulraghavankklm@gmail.com>
2018-09-20 14:32:51 +0530

    purge unused css: configured purgecss webpack plugin

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -9,6 +9,8 @@
 'use strict';
 
 const autoprefixer = require('autoprefixer');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -380,6 +382,12 @@ module.exports = {
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
       filename: cssFilename,
+    }),
+    // Remove unused css with Purgecss. See https://github.com/FullHuman/purgecss
+    // for more information about purgecss.
+    // Specify the path of the html files and source files
+    new PurgecssPlugin({
+      paths: [paths.appHtml, ...glob.sync(`${paths.appSrc}/**/*.{js,jsx,mjs}`)],
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
